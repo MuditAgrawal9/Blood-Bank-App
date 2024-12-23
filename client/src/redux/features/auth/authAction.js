0d30119e081a9d028b565235fc6b15c70efcb1c5
Mudit Agrawal <muditagrawal@Mudits-MacBook-Pro.local>
2024-12-23 17:41:58 +0530

    register functionality

--- client/src/redux/features/auth/authAction.js
+++ client/src/redux/features/auth/authAction.js
@@ -10,7 +10,7 @@ export const userLogin = createAsyncThunk(
       console.log("Sending Login Request:", { email, password, role });
 
       const { data } = await API.post("/auth/login", { role, email, password });
-      console.log("data=",{data});
+      console.log("data=", { data });
       //store token
       if (data.success) {
         localStorage.setItem("token", data.token);
@@ -21,6 +21,66 @@ export const userLogin = createAsyncThunk(
       //   }
       return data;
     } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+//register
+export const userRegister = createAsyncThunk(
+  "/auth/registrer",
+  async (
+    {
+      name,
+      role,
+      email,
+      password,
+      organizationName,
+      hospitalName,
+      address,
+      phone,
+      website,
+    },
+    { rejectWithValue }
+  ) => {
+    try {
+      console.log("Sending Register Request:", {
+        name,
+        role,
+        email,
+        password,
+        organizationName,
+        hospitalName,
+        address,
+        phone,
+        website,
+      });
+      const { data } = await API.post("/auth/register", {
+        name,
+        role,
+        email,
+        password,
+        organizationName,
+        hospitalName,
+        address,
+        phone,
+        website,
+      });
+      console.log("data=", { data });
+      if (data.success) {
+        toast.success(`${data.message} | Redirecting to Login Page`);
+        //redirect after some seconds
+        setTimeout(() => {
+          window.location.replace("/login");
+        }, 4000);
+      }
+      return data;
+    } catch (error) {
+      console.log(error);
       if (error.response && error.response.data.message) {
         return rejectWithValue(error.response.data.message);
       } else {
