6d0354cb7d365514a4c362407d5424096db5e77e
Mudit Agrawal <130285941+MuditAgrawal9@users.noreply.github.com>
2024-12-24 00:58:35 +0530

    created Protected route

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,8 +1,21 @@
 const JWT = require("jsonwebtoken");
+const colors = require("colors");
 
 module.exports = async (req, res, next) => {
   try {
-    const token = req.headers["authorization"].split(" ")[1];
+    //check if token present as if it is not present accessing it will cause error
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      return res.status(401).send({
+        success: false,
+        message: "Authorization header missing",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).send({ success: false, message: "Token missing" });
+    }
     JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
         return res.status(401).send({
@@ -11,6 +24,8 @@ module.exports = async (req, res, next) => {
         });
       } else {
         req.body.userId = decode.userId;
+        // console.log(req , decode.userId)
+        console.log(`Auth middleware success`.bgGreen.white)
         next();
       }
     });
